1a92316a1f6299fa300a50870eb25f14e990c1bb
samrajs24 <samraj464@gmail.com>
2024-03-04 08:47:02 +0530

    Added Profice Picture for Users

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include,path
 from users import views  as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,7 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('profile/',user_views.profilepage,name='profile'),
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
